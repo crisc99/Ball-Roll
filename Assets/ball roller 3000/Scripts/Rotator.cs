81bf853f7d008153bbe5572e06fdb2ff121925ba
crisc99 <crsic99knights.ucf.edu>
2019-07-05 23:53:20 -0400

    Exe is unity project 1 application file, couldnt change the name

---
+++ Assets/ball roller 3000/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Cristian Cruz
+// Rotator
+
+public class Rotator : MonoBehaviour {
+
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+}	
